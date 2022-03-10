3ed01964ff29c1491a5b06a3c3e098ffa8c279d6
Thor Hammer <thorhammer2001@gmail.com>
2022-03-10 17:09:37 +0200

    Add BitCoin Rates

---
+++ BitCoinApp Thor Hammer/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace BitCoinApp_Thor_Hammer
+{
+    partial class btnGetRates
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(btnGetRates));
+            this.CurrencyCombo = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.AmountOfCoinBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.ResultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CurrencyCombo
+            // 
+            this.CurrencyCombo.FormattingEnabled = true;
+            this.CurrencyCombo.Items.AddRange(new object[] {
+            "EUR",
+            "USD"});
+            this.CurrencyCombo.Location = new System.Drawing.Point(23, 23);
+            this.CurrencyCombo.Name = "CurrencyCombo";
+            this.CurrencyCombo.Size = new System.Drawing.Size(121, 21);
+            this.CurrencyCombo.TabIndex = 0;
+            this.CurrencyCombo.Text = "Select Currency";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(204, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Get Rates";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AmountOfCoinBox
+            // 
+            this.AmountOfCoinBox.Location = new System.Drawing.Point(23, 101);
+            this.AmountOfCoinBox.Name = "AmountOfCoinBox";
+            this.AmountOfCoinBox.Size = new System.Drawing.Size(100, 20);
+            this.AmountOfCoinBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(23, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Amount Of BitCoin Owned";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.BackColor = System.Drawing.Color.Transparent;
+            this.resultLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.resultLabel.Location = new System.Drawing.Point(20, 163);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(37, 13);
+            this.resultLabel.TabIndex = 4;
+            this.resultLabel.Text = "Result";
+            this.resultLabel.Visible = false;
+            // 
+            // ResultTextBox
+            // 
+            this.ResultTextBox.Location = new System.Drawing.Point(23, 180);
+            this.ResultTextBox.Name = "ResultTextBox";
+            this.ResultTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ResultTextBox.TabIndex = 5;
+            this.ResultTextBox.Visible = false;
+            // 
+            // btnGetRates
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(763, 419);
+            this.Controls.Add(this.ResultTextBox);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AmountOfCoinBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CurrencyCombo);
+            this.Name = "btnGetRates";
+            this.Text = "BitCoin Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CurrencyCombo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox AmountOfCoinBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox ResultTextBox;
+    }
+}
+
